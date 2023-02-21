4b6c1cbadb1fa17a15e9616daa92204435246566
Saba Chilachava <sabachilachava@icloud.com>
2023-02-21 17:17:51 +0400

    fixed carousel bug

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,12 @@
-SVGFEPointLightElement mainimg = document.querySelector('.section-image');
-let images = document.querySelectorAll('.section-carousel');
+let mainimg = document.querySelector(".section-image");
+let images = document.querySelectorAll(".section-carousel");
 
-
-
-
-console.log(mainimg.src)
-images.forEach(img=>{
-    img.addEventListener('click',()=>{
+console.log(mainimg.src);
+images.forEach((img) => {
+    img.addEventListener("click", () => {
         let tmp = mainimg.src;
         mainimg.src = img.src;
         img.src = tmp;
-        console.log(img.src)
-    })
-})
-
+        console.log(img.src);
+    });
+});
